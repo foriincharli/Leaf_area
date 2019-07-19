d475b8a07a68700e613316401cbf7834a30ba3bb
Charli <50045787+foriincharli@users.noreply.github.com>
2019-07-19 19:32:16 +1000

    Update ouroboros.py

--- ouroboros.py
+++ ouroboros.py
@@ -16,9 +16,12 @@ from datetime import datetime
 hsv_lower = (32, 60, 60)
 hsv_higher = (80, 255, 255)
 
-root = "path/to/source/images"
-out = "path/to/where/you/want/to/keep/output"
+root = "/Users/admin/Desktop/Python/Shortlist img/"
+
+out = "/Users/admin/Desktop/Python/Thresh img/"
+
 shpfiles = []
+
 for dirpath, subdirs, files in os.walk(root):
     shpfiles.extend(os.path.join(dirpath, x) for x in files if x.endswith(".jpg"))
 
@@ -28,21 +31,17 @@ for i in range(len(files)):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     hsv1 = cv2.inRange(hsv, hsv_lower, hsv_higher)
     dest = cv2.bitwise_and(img, img, mask = hsv1)
-    contours, hierarchy = cv2.findContours(hsv1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    img2, contours, hierarchy = cv2.findContours(hsv1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnt2 = cv2.drawContours(dest, contours, -1, (0, 255, 0), 4)
     newName = 'seg' + '_' + files[i].split('_')[2:][0] # remember to change the index if the file name changes! 
     cv2.imwrite(out + '/' + newName, cnt2)
-
-
-for i in range(len(contours)):
-    BW = np.uint8(np.zeros((hsv1.shape)))
-    cnt = contours[i]
-    area = cv2.contourArea(cnt)
-    if area < 10: continue    
-    print(area)
-    cv2.drawContours(BW, [cnt], 0, (255, 255, 255), -1)
-    plt.imshow(BW)
-    plt.show()
-        with open("threshed_round.csv", 'a') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow([datetime.now(), newName, area])
+    for i in range(len(contours)):
+        BW = np.uint8(np.zeros((hsv1.shape)))
+        cnt = contours[i]
+        area = cv2.contourArea(cnt)
+        if area < 50: continue    
+        # print(area)
+        cv2.drawContours(BW, [cnt], 0, (255, 255, 255), -1)
+        with open("file_1.csv", 'a') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow([datetime.now(), newName, area])
